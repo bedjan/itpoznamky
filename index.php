4e46eaa15b864e01c98184f335c171fede457356
bedjan <29821749+bedjan@users.noreply.github.com>
2024-05-18 18:26:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 copy("https://github.com/bedjan/itpoznamky/raw/main/Parsedown.php", "Parsedown.php");
 copy("https://github.com/bedjan/itpoznamky/raw/main/itnetwork.md", "itnetwork.md");
-
-?>
-
-<?php
+echo "<base target='_blank'>";
+echo "<link rel='stylesheet' href='https://unpkg.com/simpledotcss/simple.css'>";
 include('Parsedown.php');
 $html = file_get_contents('https://github.com/bedjan/itpoznamky/raw/main/itnetwork.md');
 $Parsedown = new Parsedown();
